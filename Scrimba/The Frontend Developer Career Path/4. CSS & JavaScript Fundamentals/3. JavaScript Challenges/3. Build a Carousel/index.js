94de6b2776f8d28368bc5c2e4a54879f462ed2c7
Krystian <sogestpodgorna4@gmail.com>
2021-10-08 23:55:04 +0200

    back to js, slider carousel part 999

--- Scrimba/The Frontend Developer Career Path/4. CSS & JavaScript Fundamentals/3. JavaScript Challenges/3. Build a Carousel/index.js
+++ Scrimba/The Frontend Developer Career Path/4. CSS & JavaScript Fundamentals/3. JavaScript Challenges/3. Build a Carousel/index.js
@@ -1,43 +1,30 @@
-/*jshint esversion: 6 */
-
+/*jshint esversion: 6*/
 const carousel = document.querySelector('#carousel');
 const carouselBtnPrev = document.querySelector('#carousel-btn-prev');
 const carouselBtnNext = document.querySelector('#carousel-btn-next');
 const carouselItem = document.querySelectorAll('.carousel-item');
 let count = 0;
+
 carouselBtnNext.addEventListener('click', function () {
-  console.log('P');
-  count += 1;
-  if (count == 3) {
+  if (count == carouselItem.length - 1) {
     count = 0;
-  }
+  } else count += 1;
 
-  if (count == 1) {
-    carouselItem[1].classList.add('carousel-item-visible');
-    carouselItem[0].classList.remove('carousel-item-visible');
-  } else if (count == 2) {
-    carouselItem[2].classList.add('carousel-item-visible');
-    carouselItem[1].classList.remove('carousel-item-visible');
-  } else if (count == 0) {
-    carouselItem[0].classList.add('carousel-item-visible');
-    carouselItem[2].classList.remove('carousel-item-visible');
-  }
+  hideAllSlides();
+  carouselItem[count].classList.add('carousel-item-visible');
 });
+
 carouselBtnPrev.addEventListener('click', function () {
-  console.log('P');
-  count -= 1;
-  if (count == -1) {
-    count = 2;
-  }
+  if (count > 0) {
+    count -= 1;
+  } else count = carouselItem.length - 1;
 
-  if (count == 1) {
-    carouselItem[1].classList.add('carousel-item-visible');
-    carouselItem[2].classList.remove('carousel-item-visible');
-  } else if (count == 2) {
-    carouselItem[2].classList.add('carousel-item-visible');
-    carouselItem[0].classList.remove('carousel-item-visible');
-  } else if (count == 0) {
-    carouselItem[0].classList.add('carousel-item-visible');
-    carouselItem[1].classList.remove('carousel-item-visible');
-  }
+  hideAllSlides();
+  carouselItem[count].classList.add('carousel-item-visible');
 });
+
+function hideAllSlides() {
+  for (var i = 0; i < carouselItem.length; i++) {
+    carouselItem[i].classList.remove('carousel-item-visible');
+  }
+}
