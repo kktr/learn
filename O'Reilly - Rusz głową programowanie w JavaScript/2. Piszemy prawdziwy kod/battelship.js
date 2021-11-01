ac49d04da36a8561f82d1c3922cc434becf9276a
Krystian <sogestpodgorna4@gmail.com>
2021-11-01 13:22:45 +0100

    reformating

--- O'Reilly - Rusz głową programowanie w JavaScript/2. Piszemy prawdziwy kod/battelship.js
+++ O'Reilly - Rusz głową programowanie w JavaScript/2. Piszemy prawdziwy kod/battelship.js
@@ -1,3 +1,5 @@
+/*jshint esversion:6*/
+/* eslint-env es6 */
 let randomLoc = Math.floor(Math.random() * 5);
 let location1 = randomLoc;
 let location2 = location1 + 1;
@@ -10,23 +12,28 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-  guess = prompt("Gotów, cel, pal!(podaj liczbę od 0-6):");
+  guess = prompt('Gotów, cel, pal!(podaj liczbę od 0-6):');
   if (guess < 0 || guess > 6) {
-    alert("Proszę podać prawidłowy numer komórki");
+    alert('Proszę podać prawidłowy numer komórki');
   } else {
-      guesses += 1;
-      if (guess == location1 || guess == location2 || guess == location3) {
-        alert("Trafiony!")
-        hits += 1;
-        if (hits === 3) {
-          isSunk = true;
-          alert("Zatopiłeś mój okręt!");
-        }
-      } else {
-          alert("Pudło");
-        }
+    guesses += 1;
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert('Trafiony!');
+      hits += 1;
+      if (hits === 3) {
+        isSunk = true;
+        alert('Zatopiłeś mój okręt!');
+      }
+    } else {
+      alert('Pudło');
+    }
   }
 }
-let stats = "Potrzebowałeś " + guesses + " prób, by zatopić okręt, " +
-            "czyli Twoja efektywność wynosi: " + (3/guesses) + " .";
-alert(stats)
+let stats =
+  'Potrzebowałeś ' +
+  guesses +
+  ' prób, by zatopić okręt, ' +
+  'czyli Twoja efektywność wynosi: ' +
+  3 / guesses +
+  ' .';
+alert(stats);
