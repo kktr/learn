51e871a453df9377a22f8ffdb4d85fc785955035
Krystian <sogestpodgorna4@gmail.com>
2021-11-23 10:12:15 +0100

    Scrimba 6.3 Build Pac-man in JS - progress - add keydown eventlistener - add functions: control, move - add pacMan new class Sprite

--- Scrimba/The Frontend Developer Career Path/6. Next-level JavaScript/3. Build Pac-man in JavaScript/script.js
+++ Scrimba/The Frontend Developer Career Path/6. Next-level JavaScript/3. Build Pac-man in JavaScript/script.js
@@ -7,6 +7,9 @@ let squares = [];
 let gameComponents = [];
 const width = 28;
 
+const directions = { up: -width, down: width, left: -1, right: 1 };
+const com = { pacdots: 0, wall: 1, ghostliar: 2, powerpellet: 3, emptry: 4 };
+
 const gameElements = [
   'pac-dots',
   'wall',
@@ -23,8 +26,11 @@ class Component {
   isInLoyaut(i) {
     if (layout[i] == this.number) return true;
   }
-  display(i, squares) {
+  display(i) {
     squares[i].classList.add(this.clas);
+  }
+  removeDisplay(i) {
+    squares[i].classList.remove(this.clas);
   }
 }
 
@@ -37,6 +43,57 @@ function createComponents(array) {
 }
 
 createComponents(gameElements);
+
+class Sprite extends Component {
+  constructor(number, clas, position, moveDirection) {
+    super(number, clas);
+    this.position = position;
+    this.moveDirection = moveDirection;
+  }
+  isOnTopBorder() {
+    if (this.position - width >= 0) return false;
+    else return true;
+  }
+  isOnBottomBorder() {
+    if (this.position + width < width * width) return false;
+    else return true;
+  }
+  isOnLeftBorder() {
+    if (this.position % width !== 0) return false;
+    else return true;
+  }
+  isOnRightBorder() {
+    if (this.position % width < width - 1) return false;
+    else return true;
+  }
+  isHeading(something) {
+    if (
+      squares[pacMan.position + pacMan.moveDirection].classList.contains(
+        something.clas
+      )
+    )
+      return true;
+    else return false;
+  }
+  isTryingMove(where) {
+    if (pacMan.moveDirection == where) return true;
+    else return false;
+  }
+  move(where) {
+    this.position += where;
+  }
+  moveDown() {
+    this.position += directions.down;
+  }
+  moveLeft() {
+    this.position += directions.left;
+  }
+  moveRight() {
+    this.position += directions.right;
+  }
+}
+
+pacMan = new Sprite(6, 'pacman', 490, directions.right);
 
 // prettier-ignore
 const layout = [
@@ -84,3 +141,73 @@ function createBoard() {
 }
 
 createBoard();
+
+pacMan.display(pacMan.position);
+
+function control(e) {
+  switch (event.key) {
+    case 'Down': // IE/Edge specific value
+    case 'ArrowDown':
+      console.log('down');
+      pacMan.moveDirection = directions.down;
+      console.log(pacMan.moveDirection);
+      break;
+    case 'Up': // IE/Edge specific value
+    case 'ArrowUp':
+      pacMan.moveDirection = directions.up;
+      break;
+    case 'Left': // IE/Edge specific value
+    case 'ArrowLeft':
+      pacMan.moveDirection = directions.left;
+      break;
+    case 'Right': // IE/Edge specific value
+    case 'ArrowRight':
+      pacMan.moveDirection = directions.right;
+      break;
+    default:
+      return;
+  }
+}
+
+document.addEventListener('keydown', control);
+
+function move() {
+  if (!pacMan.isHeading(gameComponents[com.wall])) {
+    if (pacMan.isTryingMove(directions.up) & !pacMan.isOnTopBorder()) {
+      console.log(!pacMan.isOnTopBorder());
+      pacMan.move(directions.up);
+    } else if (
+      (pacMan.moveDirection == directions.down) &
+      !pacMan.isOnBottomBorder()
+    ) {
+      pacMan.moveDown();
+    } else if (
+      (pacMan.moveDirection == directions.left) &
+      !pacMan.isOnLeftBorder()
+    ) {
+      pacMan.moveLeft();
+    } else if (
+      (pacMan.moveDirection == directions.right) &
+      !pacMan.isOnRightBorder()
+    ) {
+      pacMan.moveRight();
+    }
+    // for (let i = 0; i < directions.length; i++) {
+    //   if (pacMan.isTryingMove(directions[i]) & !pacMan.isOnTopBorder()) {
+    //     console.log(!pacMan.isOnTopBorder());
+    //     pacMan.move(directions[i]);
+    //   }
+    // }
+  }
+}
+setInterval(game, 1000);
+
+function game() {
+  pacMan.removeDisplay(pacMan.position);
+  move();
+  pacMan.display(pacMan.position);
+}
+
+console.log(gameComponents);
+console.log(pacMan);
+console.log(gameComponents[com.wall]);
