ace51eb7e1d3b764864dccd6fef4c4ce0a0d19f7
Krystian <sogestpodgorna4@gmail.com>
2021-11-27 13:51:03 +0100

    Scrimba 7.2 Urls and REST - done

--- Scrimba/The Frontend Developer Career Path/7. Working with APIs/2. URLs and REST/script.js
+++ Scrimba/The Frontend Developer Career Path/7. Working with APIs/2. URLs and REST/script.js
@@ -1,39 +1,71 @@
 /*jshint esversion: 6*/
 /* eslint-env es6 */
 const posts = document.getElementById('blog-posts');
-const newPost = document.getElementById('send-post');
-const inputPostBody = document.getElementById('post-body');
-const inputPostTitle = document.getElementById('post-title');
-let fiveFirstPosts = '';
+const sendNewPost = document.getElementById('send-new-post');
+const inputPostBody = document.getElementById('new-post-body');
+const inputPostTitle = document.getElementById('new-post-title');
+let displayedPosts = {};
 
 function getPosts() {
   fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
-    .then(data => saveData(data));
+    .then(data => {
+      savePosts(data);
+      displayPosts();
+    });
 }
 
 getPosts();
 
-function saveData(data) {
-  fiveFirstPosts = data.slice(0, 5);
-  console.log(fiveFirstPosts[0].body);
-  displayPosts();
+function savePosts(data) {
+  displayedPosts = data.slice(0, 5);
 }
 
 function displayPosts() {
-  fiveFirstPosts.forEach((poster, i) => {
+  posts.innerHTML = '';
+  displayedPosts.forEach((poster, i) => {
     posts.innerHTML += `
     <div class="post post-${i + 1}">
-      <h3 class= "post-title post-${i + 1}-title">
+      <h3 class= "new-post-title post-${i + 1}-title">
         ${poster.title}
       </h3>
 
-      <p class="post-body">${poster.body}</p>
+      <p class="new-post-body">${poster.body}</p>
     </div>`;
   });
 }
 
-newPost.addEventListener('submit', function(e) {
+sendNewPost.addEventListener('submit', function(e) {
   e.preventDefault();
-  console.log(inputPostBody.value);
+  const postBody = inputPostBody.value;
+  const postTitle = inputPostTitle.value;
+  this.reset();
+  const newPost = {
+    title: postTitle,
+    body: postBody
+  };
+  displayedPosts.unshift(newPost);
+  displayPosts();
+
+  fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  })
+    .then(response => response.json())
+    .then(data => console.log(data));
 });
+
+fetch(
+  'https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=51.94&lon=15.5&units=metric',
+  {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+)
+  .then(response => response.json())
+  .then(data => console.log(data));
