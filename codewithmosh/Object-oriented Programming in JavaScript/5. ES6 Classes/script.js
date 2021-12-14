2f666919c4922bd8143eafac24d0ec1e4a0ccdeb
Krystian <sogestpodgorna4@gmail.com>
2021-12-14 17:54:42 +0100

    codewithmosh - OOP in JS progress

--- codewithmosh/Object-oriented Programming in JavaScript/5. ES6 Classes/script.js
+++ codewithmosh/Object-oriented Programming in JavaScript/5. ES6 Classes/script.js
@@ -66,10 +66,149 @@ const Square = class {};
 
 // 5. Private members using sybols
 
+const _radius = Symbol('radius');
+const _draw = Symbol('draw');
+
 class Circle3 {
   constructor(radius) {
-    this.radius = radius;
+    this[_radius] = radius;
+  }
+
+  [_draw]() {
+    console.log('draw');
   }
 }
 
 const c3 = new Circle3(3);
+
+console.log(Object.getOwnPropertySymbols(c3)[0]);
+
+// 6. Private members using WaksMaps
+
+const _radius2 = new WeakMap();
+const _move = new WeakMap();
+
+class Circle4 {
+  constructor(radius) {
+    _radius2.set(this, radius);
+
+    _move.set(this, () => {
+      console.log('move4');
+    });
+  }
+
+  draw() {
+    _move.get(this)();
+
+    console.log('draw4');
+  }
+}
+
+const c4 = new Circle4(4);
+
+console.log(c4.draw());
+console.log(_radius2);
+
+// 7. Getters and setters
+
+const _radius5 = new WeakMap();
+
+class Circle5 {
+  constructor(radius) {
+    _radius5.set(this, radius);
+  }
+
+  get radius() {
+    return _radius5.get(this);
+  }
+
+  set radius(value) {
+    if (value <= 0) throw new Error('invalid radius');
+    _radius5.set(this, value);
+  }
+}
+
+const c5 = new Circle5(5);
+
+// 8. Class Inheritance
+
+class Shape8 {
+  constructor(color) {
+    this.color = color;
+  }
+
+  move() {
+    console.log('move8');
+  }
+}
+
+class Circle8 extends Shape8 {
+  constructor(color, radius) {
+    super(color);
+    this.radius = radius;
+  }
+
+  draw() {
+    console.log('draw8');
+  }
+}
+
+const c8 = new Circle8('red', 8);
+
+// 9. Method OverRiding
+
+class Shape9 {
+  move() {
+    console.log('moveshape8');
+  }
+}
+
+class Circle9 extends Shape9 {
+  move() {
+    console.log('movecircle9');
+  }
+}
+
+const c9 = new Circle9();
+
+// 10 Execrcise
+
+const _stack = new WeakMap();
+
+class Stack {
+  constructor() {}
+
+  get count() {
+    return _stack.length;
+  }
+
+  pop() {
+    if (this.count <= 0) throw new Error('stack is empty');
+    _stack.pop();
+  }
+
+  push(value) {
+    _stack.push(value);
+  }
+}
+
+const stack = new Stack();
+//
+// const _radius5 = new WeakMap();
+//
+// class Circle5 {
+//   constructor(radius) {
+//     _radius5.set(this, radius);
+//   }
+//
+//   get radius() {
+//     return _radius5.get(this);
+//   }
+//
+//   set radius(value) {
+//     if (value <= 0) throw new Error('invalid radius');
+//     _radius5.set(this, value);
+//   }
+// }
+//
+// const c5 = new Circle5(5);
