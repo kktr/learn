186eda4a46fb7f511b86b84b059401f39ec1c4e4
Krystian <sogestpodgorna4@gmail.com>
2021-12-26 09:04:35 +0100

    JS From Zero to Expert - 4. JS in Browser Dom and Events Fundamentals 2. Modal Window - DONE

---
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/4. JavaScript in the Browser DOM and Events Fundamentals/2. Modal Window/script.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 6*/
+/* eslint-env es6 */
+'use strict';
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const modalEl = document.querySelector('.modal');
+const closeModalEl = document.querySelector('.close-modal');
+const overlayEl = document.querySelector('.overlay');
+
+function displayModal() {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+}
+
+function hideModal() {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+}
+
+showModalBtns.forEach(btn => btn.addEventListener('click', displayModal));
+
+closeModalEl.addEventListener('click', hideModal);
+
+overlayEl.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && !modalEl.classList.contains('hidden')) {
+    hideModal();
+  }
+});
