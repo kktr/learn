3c97c53a3aad1557ebbde5d484b7d3412bf9525c
Krystian <sogestpodgorna4@gmail.com>
2021-12-31 12:48:41 +0100

    feat: complete lesson 9/113 Optional Chaining (?.)

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/6. Data Structures. Modern Operators and Strings/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/6. Data Structures. Modern Operators and Strings/script.js
@@ -426,7 +426,7 @@ for (const [i, el] of menu3.entries()) {
   console.log(`${i + 1}: ${el}`);
 }
 
-// 112. Enhanced Object Literals
+// 9/112 Enhanced Object Literals
 
 const weekdays3 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -456,3 +456,37 @@ const restaurant3 = {
 };
 
 console.log(restaurant3);
+
+// 9/113 Optional Chaining (?.)
+
+// error Cannot read property 'open' of undefined
+// console.log(restaurant.openingHours.mon.open);
+
+if (restaurant.openingHours.mon && restaurant.openingHours.mon.open) {
+  console.log(restaurant.openingHours.mon);
+}
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours.thu?.open);
+
+console.log(restaurant.openingHours?.mon?.open);
+
+const weekdays4 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of weekdays4) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`In ${day}, we open at ${open}`);
+}
+
+// Methods
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.car?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+
+let user = [];
+console.log(user[0]?.name ?? 'User array empty');
+
+user = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+console.log(user[0]?.name ?? 'User array empty');
