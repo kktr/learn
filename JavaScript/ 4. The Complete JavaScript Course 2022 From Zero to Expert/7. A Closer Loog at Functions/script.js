503f389d8d6151e73ff2ade2eeb474d46b62cdf3
Krystian <sogestpodgorna4@gmail.com>
2022-01-06 07:14:50 +0100

    feat: complete lesson 10/129 How passing arguments works

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
@@ -6,7 +6,7 @@
 // 10/126 Section Intro
 // 10/127 Section Roadmap
 
-// 10/128 Default Parameters
+// 10/128 Default
 
 const bookings = [];
 
@@ -26,3 +26,41 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 };
 
 createBooking('LH123');
+
+// 129 How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 235448789354,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 235448789354) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+// flight doesn't change
+console.log(flight);
+// name changes
+console.log(jonas);
+
+// Is the same as doing
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+console.log(jonas);
