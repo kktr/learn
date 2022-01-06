36d9dbbe52996389fc1b71aec569b12c9278b204
Krystian <sogestpodgorna4@gmail.com>
2022-01-06 08:07:57 +0100

    feat: complete lesson 10/132 Functions returning functions

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
@@ -96,3 +96,24 @@ const high5 = function () {
 };
 
 document.body.addEventListener('click', high5);
+
+// 10/132 Functions Returning Functions
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greet2 = (greeting) => (name) => {
+  console.log(`${greeting} ${name}`);
+};
+
+const greeterHey2 = greet2('Hey');
+greeterHey2('Jack');
