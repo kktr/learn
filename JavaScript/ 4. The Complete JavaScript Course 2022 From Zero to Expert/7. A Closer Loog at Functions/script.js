aceaa536237f19db86fa36900a95db32fb051da3
Krystian <sogestpodgorna4@gmail.com>
2022-01-06 07:47:35 +0100

    feat: complete 10/131 Functions Accepting Callback Fn

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
@@ -64,3 +64,35 @@ newPassport(jonas);
 checkIn(flight, jonas);
 
 console.log(jonas);
+
+// 10/130 First Class and Higher Order Functions
+
+// 10/131 Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  console.log(str.replace(/ /g, '').toLowerCase());
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function
+const transformer = function (str, fn) {
+  console.log(`Orginal string: ${str} `);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback all the time
+const high5 = function () {
+  console.log('🙏');
+};
+
+document.body.addEventListener('click', high5);
