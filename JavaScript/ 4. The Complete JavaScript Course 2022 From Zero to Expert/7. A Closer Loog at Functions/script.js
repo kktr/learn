c6dd8ddd67921f033dde0cd1e733fc33ae4aa91b
Krystian <sogestpodgorna4@gmail.com>
2022-01-07 06:49:00 +0100

    feat: complete lesson 10/133 The call and apply Methods

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/7. A Closer Loog at Functions/script.js
@@ -117,3 +117,57 @@ const greet2 = (greeting) => (name) => {
 
 const greeterHey2 = greet2('Hey');
 greeterHey2('Jack');
+
+// 10/133 The call and apply Methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+lufthansa.book.call(eurowings, 999, 'Jack');
+console.log(eurowings);
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams')
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'George Cooper');
+
+// Apply method (not used in modern JS)
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// Modern method
+book.call(swiss, ...flightData);
+console.log(swiss);
