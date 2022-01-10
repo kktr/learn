c66ec92eced9ab7a480f6741cba261e8b525695e
Krystian <sogestpodgorna4@gmail.com>
2022-01-10 19:52:46 +0100

    feat: progress lesson Full project refactoring

---
+++ Scrimba/The Frontend Developer Career Path/9. Learn Ui Design/3. Full project refactoring/script.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 11*/
+/* eslint-env es12 */
+
+'use strict';
+
+const get = (element) => document.getElementById(element);
+
+let open = get('menu-btn');
+let nav = get('nav');
+let exit = get('exit-btn');
+
+open.addEventListener('click', () => {
+  nav.classList.add('open-nav');
+});
+
+exit.addEventListener('click', () => {
+  nav.classList.remove('open-nav');
+});
