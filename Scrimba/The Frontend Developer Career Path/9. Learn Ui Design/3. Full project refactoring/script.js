2cdba5ad44ea3a7ef2ff8408029f44335b3a6441
Krystian <sogestpodgorna4@gmail.com>
2022-01-11 11:43:24 +0100

    feat: progress lesson Full project refactoring
    - add desktop @media

--- Scrimba/The Frontend Developer Career Path/9. Learn Ui Design/3. Full project refactoring/script.js
+++ Scrimba/The Frontend Developer Career Path/9. Learn Ui Design/3. Full project refactoring/script.js
@@ -16,3 +16,17 @@ open.addEventListener('click', () => {
 exit.addEventListener('click', () => {
   nav.classList.remove('open-nav');
 });
+
+// show viewport width
+
+let width = document.getElementById('width');
+var onresize = function () {
+  //your code here
+  //this is just an example
+  width.innerText = document.body.clientWidth;
+  width.classList.add('display-width');
+  setTimeout(() => {
+    width.classList.remove('display-width');
+  }, 2000);
+};
+window.addEventListener('resize', onresize);
