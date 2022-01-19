15dbe0c9a915b67d21e3efee514f1411bcdd626d
Krystian <sogestpodgorna4@gmail.com>
2022-01-19 10:58:11 +0100

    feat: progress lesson Build and deploy portfolio

--- Scrimba/The Frontend Developer Career Path/9. Learn Ui Design/4. Build and deploy portfolio/js/script.js
+++ Scrimba/The Frontend Developer Career Path/9. Learn Ui Design/4. Build and deploy portfolio/js/script.js
@@ -5,10 +5,18 @@
 
 const navToggleBtn = document.getElementById('nav-toggle-btn');
 const navEl = document.getElementById('nav-el');
+const navLinksEl = document.querySelectorAll('.nav__link');
 
 console.log(navEl);
 navToggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   navEl.classList.toggle('nav--visible');
   navToggleBtn.classList.toggle('nav-toggle--fixed');
+});
+
+navLinksEl.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    navEl.classList.remove('nav--visible');
+    navToggleBtn.classList.remove('nav-toggle--fixed');
+  });
 });
