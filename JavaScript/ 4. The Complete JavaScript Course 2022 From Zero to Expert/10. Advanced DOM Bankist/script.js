7150271718140b14242d6fddc297e2b01a37ce87
Krystian <sogestpodgorna4@gmail.com>
2022-01-30 09:51:57 +0100

    feat: complete lesson 13/188 Implementing smooth scrolling

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/10. Advanced DOM Bankist/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/10. Advanced DOM Bankist/script.js
@@ -124,3 +124,36 @@ logo.classList.contains('c');
 
 // don't use
 // logo.className = 'jonas'
+
+// 13/188 Implementing smooth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log('s1coords', s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  //   window.scrollTo(
+  //     s1coords.left + window.pageXOffset,
+  //     s1coords.top + window.pageYOffset
+  //   );
+  // });
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
