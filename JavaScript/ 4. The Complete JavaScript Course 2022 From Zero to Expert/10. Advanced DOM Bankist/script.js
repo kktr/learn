5fc9243ba7c2dda8455d920fac445ed497a329ea
Krystian <sogestpodgorna4@gmail.com>
2022-02-03 10:45:39 +0100

    feat: complete lesson 13/202 Lifecycle DOM events

--- JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/10. Advanced DOM Bankist/script.js
+++ JavaScript/ 4. The Complete JavaScript Course 2022 From Zero to Expert/10. Advanced DOM Bankist/script.js
@@ -479,3 +479,19 @@ slider();
 // // });
 
 // // 13/194 Build tabbed component
+
+// 13/202 Lifecycle DOM events
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});
