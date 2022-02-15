c0b907dc37d4ea4ccf43e8f451364b91f9643e89
Krystian <sogestpodgorna4@gmail.com>
2022-02-15 09:45:05 +0100

    feat: complete lesson 14/209 Prototypes

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
@@ -11,9 +11,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Never do this
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -24,3 +24,23 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+// 14/209 Prototypes
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
