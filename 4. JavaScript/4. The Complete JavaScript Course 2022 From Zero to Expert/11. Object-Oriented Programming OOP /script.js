402251ecbe9323daedf8d5a680905458a5fb0a14
Krystian <sogestpodgorna4@gmail.com>
2022-02-15 10:14:05 +0100

    feat: complete lesson 14/211 Prototypal Inheritance on Built-In Objects

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
@@ -44,3 +44,32 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+// 14/211 Prototypal Inheritance on Built-In Objects
+
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [1, 2, 3, 4, 4, 4, 5, 6];
+
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+// don't do this
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// const arrUniqueValues = arr.unique();
+
+// console.log('🚀 ~ arrUniqueValues', arrUniqueValues);
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+console.dir((x) => x + 1);
