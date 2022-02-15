2fd73b369ead55f5d110474435a2ef18e36cdd8b
Krystian <sogestpodgorna4@gmail.com>
2022-02-15 10:35:15 +0100

    feat: complete lesson 14/213 ES6 Classes

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
@@ -119,3 +119,41 @@ console.log(bmw);
 mercedes.brake();
 mercedes.accelerate();
 console.log(mercedes);
+
+// 14/213 ES6 Classes
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
