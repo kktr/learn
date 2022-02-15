375ef2481105705cdb8d411d99eaa53f4ab74513
Krystian <sogestpodgorna4@gmail.com>
2022-02-15 09:36:19 +0100

    feat: complete lesson 14/208 Constructor Functions and the new Operator

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/11. Object-Oriented Programming OOP /script.js
@@ -1,3 +1,26 @@
 /* eslint-env es12 */
 
 'use strict';
+
+// 14/208 Constructor Functions and the new Operator
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
