0eb247f9375475d99e10bb394c78c5bf85c966ab
Krystian <sogestpodgorna4@gmail.com>
2022-02-17 09:40:27 +0100

    feat: add getCurrentPosition

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/12. Mapty App: OOP, geolocation, external libraries and more/script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/12. Mapty App: OOP, geolocation, external libraries and more/script.js
@@ -12,3 +12,27 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+function displayPosition(position) {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+  const cords = [latitude, longitude];
+
+  const map = L.map('map').setView(cords, 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(cords)
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+}
+
+function getPositionError() {
+  alert('Could not get yor position');
+}
+
+navigator?.geolocation.getCurrentPosition(displayPosition, getPositionError);
