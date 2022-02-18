a07627b1eba406198df3aa1a2c6d57ce45a5c64d
Krystian <sogestpodgorna4@gmail.com>
2022-02-18 08:36:38 +0100

    feat: add submitForm

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/12. Mapty App: OOP, geolocation, external libraries and more/script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/12. Mapty App: OOP, geolocation, external libraries and more/script.js
@@ -13,12 +13,14 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 function displayPosition(position) {
   const { latitude } = position.coords;
   const { longitude } = position.coords;
   const cords = [latitude, longitude];
 
-  const map = L.map('map').setView(cords, 13);
+  map = L.map('map').setView(cords, 13);
 
   L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution:
@@ -30,22 +32,64 @@ function displayPosition(position) {
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 
-  const displayClickedPosition = (mapEvent) => {
-    const { lat: latitude, lng: longitude } = mapEvent.latlng;
-    const cords = [latitude, longitude];
-    const marker = L.marker(cords)
-      .addTo(map)
-      .bindPopup(
-        L.popup({
-          maxWidth: 250,
-          minWidth: 150,
-          autoClose: false,
-          closeOnClick: false,
-          className: 'running-popup',
-        })
-      )
-      .setPopupContent('Workout')
-      .openPopup();
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const isInputValid = () => {
+      return (
+        inputDistance.value > 0 &&
+        inputDuration.value > 0 &&
+        (inputCadence.value > 0 || inputElevation.value > 0)
+      );
+    };
+
+    console.log(isInputValid());
+    if (isInputValid()) {
+      const { lat: latitude, lng: longitude } = mapEvent.latlng;
+      const cords = [latitude, longitude];
+      const capitalize = (s) => {
+        return s[0].toUpperCase() + s.slice(1);
+      };
+
+      const exerciseClass = inputType.value;
+      const exerciseType = capitalize(exerciseClass);
+      const workoutEmoticon = exerciseClass == 'running' ? '🏃‍♂️' : '🚴‍♀️';
+      const today = new Date();
+      const dateString = today.toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'long',
+      });
+
+      const marker = L.marker(cords)
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 150,
+            autoClose: false,
+            closeOnClick: false,
+            className: `${exerciseClass}-popup`,
+          })
+        )
+        .setPopupContent(`${workoutEmoticon} ${exerciseType} on ${dateString}`)
+        .openPopup();
+
+      inputDistance.value = '';
+      inputDuration.value = '';
+      inputCadence.value = '';
+      inputElevation.value = '';
+
+      return;
+    } else alert('Inputs have to be positive numbers!');
+  };
+
+  const displayClickedPosition = (mapE) => {
+    mapEvent = mapE;
+
+    form.classList.remove('hidden');
+    inputDistance.focus();
+
+    form.addEventListener('submit', submitForm);
   };
 
   map.on('click', displayClickedPosition);
@@ -54,5 +98,10 @@ function displayPosition(position) {
 function getPositionError() {
   alert('Could not get yor position');
 }
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
 
 navigator?.geolocation.getCurrentPosition(displayPosition, getPositionError);
