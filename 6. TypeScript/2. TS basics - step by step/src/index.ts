25e91951d065f9cff4b4753e13eb0a33e5447950
Krystian <sogestpodgorna4@gmail.com>
2022-03-12 09:56:35 +0100

    feat: add class Task

--- 6. TypeScript/2. TS basics - step by step/src/index.ts
+++ 6. TypeScript/2. TS basics - step by step/src/index.ts
@@ -1,1 +1,19 @@
-console.log('test3');
+const taskNameInputEl = document.querySelector('#task-name');
+const addButtonEl = document.querySelector('.form__submit-button');
+const taskContainer = document.querySelector('.task-container');
+
+class Task {
+  name;
+  category;
+  doneStatus;
+
+  constructor(
+    name: string,
+    category: Category = Category.General,
+    doneStatus: boolean
+  ) {
+    this.name = name;
+    this.category = category;
+    this.doneStatus = doneStatus;
+  }
+}
