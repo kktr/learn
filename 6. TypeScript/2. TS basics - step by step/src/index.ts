63ffac34ee50362f595cc543b346c652b36e3d7a
Krystian <sogestpodgorna4@gmail.com>
2022-03-13 12:21:25 +0100

    feat: add imports

--- 6. TypeScript/2. TS basics - step by step/src/index.ts
+++ 6. TypeScript/2. TS basics - step by step/src/index.ts
@@ -1,3 +1,13 @@
+import { Category } from './store/types.js';
+import { Task } from './classes/task.js';
+import { renderTasks } from './helpers/render-tasks.helper.js';
+import {
+  addButtonEl,
+  categoriesRadioButtons,
+  inputTaskEl,
+  createTodoEl,
+} from './helpers/create-todo.helper.js';
+
 const tasks: Task[] = [
   new Task('Learn Typescript', Category.Programing),
   new Task('Learn React', Category.Programing),
