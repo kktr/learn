51272315c5faa37a405d58a45e266781154ecbba
Krystian <sogestpodgorna4@gmail.com>
2022-03-13 13:25:38 +0100

    feat: create .eslintrc.js

---
+++ 6. TypeScript/1. Understanding Typescript - Udemy/14. React and Typescript/my-app/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'jest',
+    'promise',
+    'unicorn',
+    'import',
+  ],
+  extends: [
+    // 'airbnb-base',
+    // 'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:eslint-comments/recommended',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.eslint.json',
+  },
+};
