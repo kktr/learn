e9b19a96721822ab1e428680be85e0055ab6a163
Krystian <sogestpodgorna4@gmail.com>
2022-03-13 12:20:15 +0100

    refactor: delete taskNameInputEl, addButtonEl, taskContainer, Task

--- 6. TypeScript/2. TS basics - step by step/src/index.ts
+++ 6. TypeScript/2. TS basics - step by step/src/index.ts
@@ -1,23 +1,3 @@
-const taskNameInputEl = document.querySelector('#task-name');
-const addButtonEl = document.querySelector('.form__submit-button');
-const taskContainer = document.querySelector('.task-container');
-
-class Task {
-  name;
-  category;
-  doneStatus;
-
-  constructor(
-    name: string,
-    category: Category = Category.General,
-    doneStatus: boolean = false
-  ) {
-    this.name = name;
-    this.category = category;
-    this.doneStatus = doneStatus;
-  }
-}
-
 const tasks: Task[] = [
   new Task('Learn Typescript', Category.Programing),
   new Task('Learn React', Category.Programing),
