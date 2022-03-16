19720bfce8b8a38a1570ce86656594a7cce42237
Krystian <sogestpodgorna4@gmail.com>
2022-03-16 09:17:56 +0100

    feat: complete lesson 6/83 Intersection types

---
+++ 6. TypeScript/1. Understanding Typescript - Udemy/6. Advanced Types/src/app.ts
@@ -0,0 +1,25 @@
+// 6/83 Intersection types
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+// interface ElevatedEmployee extends Employee, Admin {}
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
