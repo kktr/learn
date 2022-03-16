6c34de5e278ad918fb93963445dd7a1ce0e35e44
Krystian <sogestpodgorna4@gmail.com>
2022-03-16 12:52:24 +0100

    feat: complete lesson /7/94 Built-in Generics

--- 6. TypeScript/1. Understanding Typescript - Udemy/7. Generics/src/app.ts
+++ 6. TypeScript/1. Understanding Typescript - Udemy/7. Generics/src/app.ts
@@ -1,1 +1,11 @@
 // 7/94 Built-in Generics & What are Generics?
+
+// Array<T> - Generic
+const names: Array<string> = ['Stachu', 'Janusz'];
+const names2: string[] = ['Stachu', 'Janusz'];
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is done');
+  }, 2000);
+});
