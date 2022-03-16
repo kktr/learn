b03464c1d850bb2dfeb5de009cbdba6c57eb2ef0
Krystian <sogestpodgorna4@gmail.com>
2022-03-16 13:20:22 +0100

    feat: complete lesson 7/97 Another generic function

--- 6. TypeScript/1. Understanding Typescript - Udemy/7. Generics/src/app.ts
+++ 6. TypeScript/1. Understanding Typescript - Udemy/7. Generics/src/app.ts
@@ -32,3 +32,21 @@ console.log(merge({ name: 'Max' }, { age: 30 }));
 // error
 // const mergedObj2 = merge2({ name: 'Max' }, 30);
 // console.log(mergedObj2.age);
+
+//  7/97 Another generic function
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there'));
