544a3fc82c4271799c95460aa33d35a8a02a6cb4
Krystian <sogestpodgorna4@gmail.com>
2022-03-16 13:05:11 +0100

    feat: complete lesson 7/96 Working with constrains

--- 6. TypeScript/1. Understanding Typescript - Udemy/7. Generics/src/app.ts
+++ 6. TypeScript/1. Understanding Typescript - Udemy/7. Generics/src/app.ts
@@ -20,3 +20,15 @@ console.log(merge({ name: 'Max' }, { age: 30 }));
 
 const mergedObj = merge({ name: 'Max' }, { age: 30 });
 console.log(mergedObj.age);
+
+// 7/96 Working with constrains
+
+function merge2<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+console.log(merge({ name: 'Max' }, { age: 30 }));
+
+// error
+// const mergedObj2 = merge2({ name: 'Max' }, 30);
+// console.log(mergedObj2.age);
