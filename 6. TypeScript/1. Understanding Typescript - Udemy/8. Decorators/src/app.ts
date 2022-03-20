ecca9ba663777f1c7bf5bb9172e92dc4b9927ecc
Krystian <sogestpodgorna4@gmail.com>
2022-03-20 16:40:49 +0100

    feat: complete lesson 8/115 Validation decorators 1

--- 6. TypeScript/1. Understanding Typescript - Udemy/8. Decorators/src/app.ts
+++ 6. TypeScript/1. Understanding Typescript - Udemy/8. Decorators/src/app.ts
@@ -156,3 +156,30 @@ const print2 = new Printer();
 const buttonEl = document.querySelector('button')!;
 
 buttonEl.addEventListener('click', print2.showMessage);
+
+// 8/115 Validation with Decorators First Steps
+
+function Required() {}
+function PositiveNumber() {}
+function validate(obj: object) {}
+class Course {
+  title: string;
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+const courseForm = document.querySelector('form');
+courseForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  console.log(createdCourse);
+});
