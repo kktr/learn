4585b168fe566d1e0eb6127f076f5b7eb5cb7863
Krystian <sogestpodgorna4@gmail.com>
2022-03-23 22:29:08 +0100

    feat: complete lesson 16/250 Callback Hell

--- 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/13. Asynchronous JavaScript: Promises Async/script.js
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/13. Asynchronous JavaScript: Promises Async/script.js
@@ -46,3 +46,76 @@ const getCountryByName = (name) => {
 
 // getCountryByName('portugal');
 // getCountryByName('usa');
+
+// 16/249 How the Web Works: Requests and Responses
+
+// 16/250 Welcome to Callback Hell
+const renderCountry = (data, className = '') => {
+  const html = document.createElement('article');
+  html.className = `country ${className}`;
+
+  const getLanguageEntry = () => {
+    return Object.entries(data.languages)[0][0];
+  };
+
+  const getCurrenciesEntry = () => {
+    return Object.entries(data.currencies)[0][0];
+  };
+
+  const language = data.languages[`${getLanguageEntry()}`];
+  const currency = data.currencies[`${getCurrenciesEntry()}`].name;
+
+  html.innerHTML = `
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${language}</p>
+    <p class="country__row"><span>💰</span>${currency}</p>
+  </div>`;
+
+  countriesContainer.insertAdjacentElement('beforeend', html);
+
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryByNameAndNeighbor = (name) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+
+    const [neighbor] = data.borders;
+
+    if (!neighbor) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbor');
+
+      const [neighbor2] = data2.borders;
+
+      if (!neighbor2) return;
+
+      const request3 = new XMLHttpRequest();
+      request3.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor2}`);
+      request3.send();
+
+      request3.addEventListener('load', function () {
+        const [data3] = JSON.parse(this.responseText);
+        renderCountry(data3, 'neighbor');
+      });
+    });
+  });
+};
+
+// getCountryByNameAndNeighbor('portugal');
+// getCountryByNameAndNeighbor('poland');
