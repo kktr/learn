923195516a94a409d791630d929a512806db1791
Krystian <sogestpodgorna4@gmail.com>
2022-03-23 22:27:31 +0100

    init section 16. Asynchronous JS

---
+++ 4. JavaScript/4. The Complete JavaScript Course 2022 From Zero to Expert/13. Asynchronous JavaScript: Promises Async/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// const { createElement } = require('parse5/lib/tree-adapters/default');
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
